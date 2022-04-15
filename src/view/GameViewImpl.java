ebaf776d933a6d8f56053aaf6b56c817e9d6effa
BooraKaushik <87960952+BooraKaushik@users.noreply.github.com>
2022-04-15 16:27:02 -0400

    resolved checkstyle errors

--- src/view/GameViewImpl.java
+++ src/view/GameViewImpl.java
@@ -1,14 +1,22 @@
 package view;
 
 import java.awt.FlowLayout;
-
 import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 
+/**
+ * Creates a new Game frame by implementing Game View.
+ */
 public class GameViewImpl extends JFrame implements GameView {
+  private static final long serialVersionUID = -2179965453492637485L;
 
+  /**
+   * Constructor For GameViewImpl class, creates a frame.
+   * 
+   * @param title String to appear on top of the window.
+   */
   public GameViewImpl(String title) {
     super(title);
 
@@ -19,16 +27,16 @@ public class GameViewImpl extends JFrame implements GameView {
     this.setLayout(null);
 
     JMenu menu = new JMenu("File");
-    JMenuBar menuBar = new JMenuBar();
 
     JMenuItem newGame = new JMenuItem("New Game");
     JMenuItem newWorld = new JMenuItem("New World");
     JMenuItem exit = new JMenuItem("Exit");
-    
+
     menu.add(newGame);
     menu.add(newWorld);
     menu.add(exit);
-    
+
+    JMenuBar menuBar = new JMenuBar();
     menuBar.add(menu);
     setJMenuBar(menuBar);
 
