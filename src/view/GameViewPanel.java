4b1b83ba25f0e1337d44c80b4a53e3ed5d56be0e
BooraKaushik <87960952+BooraKaushik@users.noreply.github.com>
2022-04-24 13:56:01 -0400

    resolved CheckStyle

--- src/view/GameViewPanel.java
+++ src/view/GameViewPanel.java
@@ -17,7 +17,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Consumer;
-import java.util.function.Function;
 import java.util.stream.Collectors;
 import javax.swing.BoxLayout;
 import javax.swing.ImageIcon;
